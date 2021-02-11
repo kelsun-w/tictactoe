288d3aedc6a88ebb3acb62c3ff498966aca9f292
kelsun-w <keldenwfinso@gmail.com>
2021-02-11 22:54:07 +0600

    :refactor code improve logic

--- script.js
+++ script.js
@@ -1,52 +1,62 @@
 const GAMEBOARD = (() => {
-    let gameboard = new Array(9);
+    let gameboard = [
+        ['','',''],
+        ['','',''],
+        ['','','']
+    ]
 
     function reset() {
-        for(let i = 0; i<gameboard.length; i++) {
-            gameboard[i] = null;
+        for (let i = 0; i < gameboard.length; i++) {
+            for (let j = 0; j < gameboard[i].length; j++) {
+                gameboard[i][j] = '';
+            }
         }
     }
-
-    function checkTile(index, player){
-        if(!gameboard[index]){
-            gameboard[index] = player;
-            return true;
+    
+    function getEmptyTiles() {
+        let vacant = [];
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if(gameboard[i][j] == '') vacant.push({i,j});
+            }
         }
-
-        return false;
+        return vacant;
     }
 
     return {
         gameboard,
         reset,
-        checkTile
+        getEmptyTiles
     }
 })();
 
-class Player{
+class Player {
     isHuman;
     check_type;
 
-    constructor(isHuman, check_type){
+    constructor (isHuman, check_type) {
         this.isHuman = isHuman;
         this.check_type = check_type;
     }
 
-    computerMove(valid_moves){
-        let move = Math.floor(Math.random()*valid_moves.length) 
-        return valid_moves[move]; 
+    computerMove(options) {
+        let move = options[Math.floor(Math.random()*options.length)];
+        return {
+            x : move.i,
+            y : move.j
+        }
     }
-};
+}
 
 const CONTROLLER = (() => {
     let dom_board;
     let msg_Modal;
     let modal_overlay;
-    let gameover;
     let gamescreen;
     let gameboard;
     let p1;
     let p2;
+    let currPlayer;
     let round;
 
     //Game state controls
@@ -57,8 +67,6 @@ const CONTROLLER = (() => {
     function init() {
         gameboard = GAMEBOARD.gameboard;
         gamescreen = 1;
-        round = 1;
-        gameover = false;
         dom_board = document.querySelector('#gameboard');
         msg_Modal = document.querySelector('#msg_modal');
         modal_overlay = document.querySelector('.modal_overlay');
@@ -75,54 +83,44 @@ const CONTROLLER = (() => {
 
     function checkWin() {
         let winner = checkRow() || checkCol() || checkDiag();
-        if(!winner) return false;
-        gameover = true;
-        endGame(winner);
+        if (winner == null && round == 9) {
+            return 'draw';
+        } else {
+            return winner;
+        }
     }
 
-    function checkRow(){
-        const WIN_CON = [[0,1,2],[3,4,5],[6,7,8]];
-        let winner = null;
-        let subset;
-        for(let i = 0; i<WIN_CON.length; i++){
-            subset = [];
-            for(let j=0; j<WIN_CON[i].length; j++){
-                subset.push(gameboard[WIN_CON[i][j]]);
-            }
-            if(subset.every((elem) => elem === 1)) winner = 1;
-            else if(subset.every((elem) => elem === 2)) winner = 2;
-        }
-        return winner;
+    function equals3(a, b, c) {
+        return a == b && b == c && a != '';
     }
 
-    function checkCol(){
-        const WIN_CON = [[0,3,6],[1,4,7],[2,5,8]]
-        let winner = null;
-        let subset;
-        for(let i = 0; i<WIN_CON.length; i++){
-            subset = [];
-            for(let j=0; j<WIN_CON[i].length; j++){
-                subset.push(gameboard[WIN_CON[i][j]]);
+    function checkRow() {
+        for (let i = 0; i < 3; i++) {
+            if (equals3(gameboard[i][0], gameboard[i][1], gameboard[i][2])) {
+                return gameboard[i][0];
             }
-            if(subset.every((elem) => elem === 1)) winner = 1;
-            else if(subset.every((elem) => elem === 2)) winner = 2;
-        }
-        return winner;
+        } 
+        return null;
     }
 
-    function checkDiag(){
-        const WIN_CON = [[0,4,8],[2,4,6]]
-        let winner = null;
-        let subset;
-        for(let i = 0; i<WIN_CON.length; i++){
-            subset = [];
-            for(let j=0; j<WIN_CON[i].length; j++){
-                subset.push(gameboard[WIN_CON[i][j]]);
+    function checkCol() {
+        for (let i = 0; i < 3; i++) {
+            if (equals3(gameboard[0][i],gameboard[1][i],gameboard[2][i])) {
+                return gameboard[0][i];
             }
-            if(subset.every((elem) => elem === 1)) winner = 1;
-            else if(subset.every((elem) => elem === 2)) winner = 2;
         }
-        return winner;
+        return null;
+    }
+
+    function checkDiag() {
+        for (let i = 0; i < 3; i++) {
+            if (equals3(gameboard[0][0], gameboard[1][1],gameboard[2][2])) {
+                return gameboard[0][0];
+            } else if (equals3(gameboard[0][2], gameboard[1][1], gameboard[2][0])) {
+                return gameboard[0][2]; 
+            }
+        }
+        return null;
     }
 
     function endGame(winner) {
@@ -140,39 +138,40 @@ const CONTROLLER = (() => {
         modal_overlay.style.display = "block";
     }
 
-    function playerMove(selectedTile, currPlayer){
-        let isValid = GAMEBOARD.checkTile(selectedTile, currPlayer);
-        if(isValid){
-            if(!gameover){
-                if(round === 9) {
-                    //It's a draw
-                    endGame(null);
-                }else{
-                    //Moving on to next player's turn
-                    ++round;
-                    nextRound();
-                }
-            }
+    function playerMove (x, y) {
+        if (gameboard[x][y] == '') {
+            gameboard[x][y] = currPlayer.check_type;
+            nextRound();
         }
     }
 
-    function nextRound(){
-        //console.log("current round"+round);
+    function nextRound() {
+        // We evaluate the previous round first before moving on to the next
+        render();
+        let result = checkWin();
+        if (result != null) {
+            // We got a game over scenario
+            if (result == 'draw') {
+                endGame();
+            } else {
+                endGame(result);
+            }
+            // Game over. End game loop
+            return;
+        }
+        
+        round++;
+        // Determine the current player based on the current round no.
         let currPlayerNo = Math.ceil(round%2);
-        let currPlayer = currPlayerNo !== 0 ? p1 : p2;
-        if(currPlayer.isHuman){
-            render(currPlayerNo === 0 ? 2 : 1);
-        }else{
-            let vacant_tiles = [];
-            for(let i=0; i<gameboard.length; i++){
-                if(!gameboard[i]) vacant_tiles.push(i);
-            }
-            let tile_no = currPlayer.computerMove(vacant_tiles);
-            playerMove(tile_no, currPlayerNo === 0 ? 2 : 1);
+        currPlayer = currPlayerNo !== 0 ? p1 : p2;
+        if (!currPlayer.isHuman) {
+            let vacant_tiles = GAMEBOARD.getEmptyTiles();  
+            let move = currPlayer.computerMove(vacant_tiles);
+            playerMove(move.x, move.y);
         }
     }
 
-    function render(currPlayer=0) {
+    function render() {
         let menu_screen = document.querySelector('#menu');
         let main_screen = document.querySelector('#main');
 
@@ -182,49 +181,40 @@ const CONTROLLER = (() => {
         }else if(gamescreen === 2){
             menu_screen.style.display = "none";
             main_screen.style.display = "flex";
-            displayBoard(currPlayer);
+            displayBoard();
         }
     }
 
-    function displayBoard(currPlayer){
-        checkWin();
+    function displayBoard(){
         console.log(gameboard);
         //Erasing prev state
         dom_board.innerHTML = "";
-        for(let i = 0; i < gameboard.length; i++) {
-            let board_tile = document.createElement('div');
-            board_tile.classList.add('gameboard-tile');
-            board_tile.addEventListener('click', () => playerMove(i, currPlayer));
+        for (let i = 0; i < 3; i++) {
+            for(let j = 0; j < 3; j++) {
+                let board_tile = document.createElement('div');
+                board_tile.classList.add('gameboard-tile');
+                board_tile.addEventListener('click', () => playerMove(i,j));
+                if(gameboard[i][j] != '')
+                    board_tile.innerText = gameboard[i][j];
 
-            if(gameboard[i]){
-                switch(gameboard[i]){
-                    case 1 :
-                        board_tile.innerText = p1.check_type;
-                        break;
-                    case 2 :
-                        board_tile.innerText = p2.check_type;
-                        break;
-                    default: break
-                }
-            }
-            dom_board.appendChild(board_tile);
-        };
+                dom_board.appendChild(board_tile);
+            };
+        }
     }
 
     function restartGame(){
         GAMEBOARD.reset();
-        gameover = false;
         round = 1;
         msg_Modal.style.display = "none";
         msg_Modal.innerHTML = "";
         modal_overlay.style.display = "none";
-        render(1);
+        currPlayer = p1;
+        render();
     }
 
     function resetGame(){
         GAMEBOARD.reset();
         gamescreen = 1;
-        gameover = false;
         round = 1;
         p1 = null;
         p2 = null;
@@ -234,10 +224,11 @@ const CONTROLLER = (() => {
     function startGame(){
         gamescreen = 2;
         var option= document.querySelector('input[name = "selection-p"]:checked').value;
-        console.log(option);
         p1 = new Player(true, 'X');
         p2 = new Player((option === "human"), 'O');
-        render(1);
+        currPlayer = p1;
+        round = 1;
+        render();
     }
 
     return {
